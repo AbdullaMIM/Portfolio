763595468761484ce66cc1b40b2cb95617c32225
AbdullaMIM <abdulhez10@gmail.com>
2023-02-26 13:26:45 +0530

    Update Navbar Component & Add Home Component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,9 @@
 import React, {useState} from 'react'
 import Logo from '../assets/MALOGO_1.png'
-import { FaBars, FaTimes } from 'react-icons/fa'
+import CV from '../assets/Abdulla_Resume.pdf'
+import { FaBars, FaTimes, FaGithub, FaLinkedin } from 'react-icons/fa'
+import {HiOutlineMail} from 'react-icons/hi'
+import {BsFillPersonLinesFill} from 'react-icons/bs'
 
 const Navbar = () => {
    
@@ -41,8 +44,33 @@ const Navbar = () => {
       </ul>
 
       {/* Social Icons */}
-      <div className='hidden'>
-         
+      <div className='hidden lg:flex fixed flex-col top-[35%] left-0'>
+         <ul>
+            <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-500'>
+                <a className='flex justify-between items-center w-full text-gray-300' href="https://www.linkedin.com/in/mohamed-abdulla-4126841b8">
+                    Linkedin<FaLinkedin size={30}/>
+                </a>
+            </li>
+
+            <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#333333]'>
+                <a className='flex justify-between items-center w-full text-gray-300' href="https://github.com/AbdullaMIM">
+                    Github<FaGithub size={30}/>
+                </a>
+            </li>
+
+            <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#16a34a]'>
+                <a className='flex justify-between items-center w-full text-gray-300' href="mailto: mohamedabdulla9823@gmail.com">
+                    Email<HiOutlineMail size={30}/>
+                </a>
+            </li>
+
+            <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#cc3434]'>
+                <a className='flex justify-between items-center w-full text-gray-300' href={CV} download={"Abdulla_Resume"}>
+                    Resume<BsFillPersonLinesFill size={30}/>
+                </a>
+            </li>
+
+         </ul>
       </div>
 
 
