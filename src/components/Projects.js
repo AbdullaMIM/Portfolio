83a6e4cb1dc1c834d219ea660db73fdcb58ed2d2
AbdullaMIM <abdulhez10@gmail.com>
2023-02-27 19:51:34 +0530

    Remove unwanted files & texts

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -27,13 +27,32 @@ const Projects = () => {
                 {/* Hover Effects */}
                 <div className='opacity-0 group-hover:opacity-100 text-center'>
                     <span className='text-2xl font-bold text-white tracking-wide'>
-                        Travel Application
+                        Travel Lanka
+                        <p className='text-xl bg-[#ffb915] text-center px-1 py-1 my-1'>React | CSS</p>
                     </span>
                     <div className='pt-8 text-center'>
-                        <a href="https://tourlanka.netlify.app" target="_blank" rel="noreferrer">
+                        <a href="https://travellankaplc.netlify.app" target="_blank" rel="noreferrer">
                             <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
                         </a>
                         <a href="https://github.com/AbdullaMIM/Travel-App" target="_blank" rel="noreferrer">
+                            <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
+                        </a>
+                    </div>
+                </div>
+              </div>
+
+              <div style={{backgroundImage: `url(${scImg})`}}  className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
+                {/* Hover Effects */}
+                <div className='opacity-0 group-hover:opacity-100 text-center'>
+                    <span className='text-2xl font-bold text-white tracking-wide'>
+                        Tech Firm
+                        <p className='text-xl bg-[#ffb915] text-center px-1 py-1 my-1'>React | Tailwindcss</p>
+                    </span>
+                    <div className='pt-8 text-center'>
+                        <a href="https://powersoftplc.netlify.app/" target="_blank" rel="noreferrer">
+                            <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
+                        </a>
+                        <a href="https://github.com/AbdullaMIM/Tech-Company" target="_blank" rel="noreferrer">
                             <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
                         </a>
                     </div>
@@ -45,12 +64,10 @@ const Projects = () => {
                 <div className='opacity-0 group-hover:opacity-100 text-center'>
                     <span className='text-2xl font-bold text-white tracking-wide'>
                         Task Management System 
+                        <p className='text-xl bg-[#ffb915] text-center px-1 py-1 my-1 mx-12'>MERN</p>
                     </span>
                     <div className='pt-8 text-center'>
-                        <a href="/" target="_blank" rel="noreferrer">
-                            <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
-                        </a>
-                        <a href="/" target="_blank" rel="noreferrer">
+                        <a href="https://github.com/AbdullaMIM/Mern-Task-Manager" target="_blank" rel="noreferrer">
                             <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
                         </a>
                     </div>
@@ -62,11 +79,9 @@ const Projects = () => {
                 <div className='opacity-0 group-hover:opacity-100 text-center'>
                     <span className='text-2xl font-bold text-white tracking-wide'>
                         Inventory Management System
+                        <p className='text-xl bg-[#ffb915] text-center px-1 py-1 my-1 mx-12'>MERN</p>
                     </span>
                     <div className='pt-8 text-center'>
-                        <a href="/" target="_blank" rel="noreferrer">
-                            <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
-                        </a>
                         <a href="https://github.com/AbdullaMIM/Inventory-Mangement-System" target="_blank" rel="noreferrer">
                             <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
                         </a>
@@ -74,35 +89,21 @@ const Projects = () => {
                 </div>
               </div>
 
-              <div style={{backgroundImage: `url(${scImg})`}}  className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
-                {/* Hover Effects */}
-                <div className='opacity-0 group-hover:opacity-100 text-center'>
-                    <span className='text-2xl font-bold text-white tracking-wide'>
-                        Frontend Design of Software Company
-                    </span>
-                    <div className='pt-8 text-center'>
-                        <a href="https://powersoftplc.netlify.app/" target="_blank" rel="noreferrer">
-                            <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
-                        </a>
-                        <a href="https://github.com/AbdullaMIM/React-Tailwind" target="_blank" rel="noreferrer">
-                            <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
-                        </a>
-                    </div>
-                </div>
-              </div>
+              
 
               <div style={{backgroundImage: `url(${pmsImg})`}}  className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
                 {/* Hover Effects */}
                 <div className='opacity-0 group-hover:opacity-100 text-center'>
                     <span className='text-2xl font-bold text-white tracking-wide'>
-                        Power Mangement Sytem - ElectroGrid
+                        Power Management System
+                        <p className='text-xl bg-[#ffb915] text-center px-1 py-1 my-1 mx-12'>Java | AJAX | MySQL</p>
                     </span>
                     <div className='pt-8 text-center'>
-                        <a href="/" target="_blank" rel="noreferrer">
-                            <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
+                        <a href="https://github.com/DestructivePeach/PAF-Project" target="_blank" rel="noreferrer">
+                            <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Backend</button>
                         </a>
-                        <a href="/" target="_blank" rel="noreferrer">
-                            <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
+                        <a href="https://github.com/AbdullaMIM/IT20056520-PAF-ASSIGNMENT-2022" target="_blank" rel="noreferrer">
+                            <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Frontend</button>
                         </a>
                     </div>
                 </div>
@@ -113,6 +114,7 @@ const Projects = () => {
                 <div className='opacity-0 group-hover:opacity-100 text-center'>
                     <span className='text-2xl font-bold text-white tracking-wide'>
                         Ecommerce Application - UI Design
+                        <p className='text-xl bg-[#ffb915] text-center px-1 py-1 my-1 mx-12'>Figma</p>
                     </span>
                     <div className='pt-8 text-center'>
                         <a href="https://www.figma.com/file/fhBw97kNDJLHvAHOAakMvV/HCI-2022?node-id=409%3A7595&t=6EULlkJaxQfQ20mk-1" target="_blank" rel="noreferrer">
